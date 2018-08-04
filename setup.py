078ea031dbb93bb1c7e98ea28b4b0a3c360729a0
Michon van Dooren <michon1992@gmail.com>
2018-08-04 21:54:21 +0000

    Initial setup

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+	name = 'pytest-depends',
+	version = '0.1.0',
+	license = 'MIT',
+	description = "Tests that depend on other tests",
+	author = 'Michon van Dooren',
+	author_email = 'michon1992@gmail.com',
+	url = 'https://gitlab.com/maienm/pytest-depends',
+	classifiers = [
+		'Development Status :: 4 - Beta',
+		'Framework :: Pytest',
+		'Intended Audience :: Developers',
+		'Topic :: Software Development :: Testing',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 2',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
+		'Programming Language :: Python :: Implementation :: CPython',
+		'Programming Language :: Python :: Implementation :: PyPy',
+		'Operating System :: OS Independent',
+		'License :: OSI Approved :: MIT License',
+	],
+	keywords = [
+		'pytest',
+	],
+
+	packages = find_packages('src'),
+	package_dir = { '': 'src' },
+	zip_safe = False,
+	install_requires = ['pytest', 'xnetwork'],
+	entry_points={
+		'pytest11': [
+			'depends = pytest_depends',
+		],
+	},
+)
